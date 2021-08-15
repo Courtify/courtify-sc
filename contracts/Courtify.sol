20e721f6d36eab91708371015f47241c7a0c46cf
Shebin John <admin@remedcu.com>
2021-08-15 17:34:09 +0530

    Documentation Added

--- contracts/Courtify.sol
+++ contracts/Courtify.sol
@@ -4,6 +4,7 @@ pragma experimental ABIEncoderV2;
 import "./CourtifyRights.sol";
 
 contract Courtify is CourtifyRights {
+
 	/* STORAGE */
 
 	uint256 public lastCaseID;
@@ -30,7 +31,15 @@ contract Courtify is CourtifyRights {
 	/* EVENTS */
 
 	/*
-	 *
+	 * @notice Emitted when case is created.
+     * @param _initiator The one who initiates the event.
+     * @param _caseID The new case id which was created.
+     * @param _timestamp The time when the case was created.
+     * @param _state The state where the case was created.
+     * @param _district The district where the case was created.
+     * @param _court The court where the case was created.
+     * @param _petitonerID The Petitioner ID.
+     * @param _caseType The case type.
 	 */
 	event CaseCreated(
 		address _initiator,
@@ -44,14 +53,18 @@ contract Courtify is CourtifyRights {
 	);
 
 	/*
-	 *
+	 * @notice Emitted when the case is created.
+     * @param _initiator The one who initiates the event.
+     * @param _caseID The case ID for which the evidence was added.
+     * @param _IPFS The IPFS hash.
 	 */
 	event EvidenceAdded(address _initiator, uint256 _caseID, string _IPFS);
 
 	/* CONSTRUCTOR */
 
 	/*
-	 *
+     * @param _chiefJustice The Chief Justice Address.
+     * @dev _chiefJustice cannot be a zero address.
 	 */
 	constructor(address _chiefJustice) public CourtifyRights(_chiefJustice) {
 		lastCaseID = 1;
@@ -61,7 +74,15 @@ contract Courtify is CourtifyRights {
 	/* PUBLIC */
 
 	/*
-	 *
+	 * @notice Function to create a new case.
+     * @param _timestamp The time when the case was created.
+     * @param _state The state where the case was created.
+     * @param _district The district where the case was created.
+     * @param _court The court where the case was created.
+     * @param _petitonerID The Petitioner ID.
+     * @param _name The Petitioner Name.
+     * @param _caseType The case type.
+     * @return _newCaseID The new case id which was created.
 	 */
 	function createNewCase(
 		uint256 _timestamp,
@@ -71,12 +92,14 @@ contract Courtify is CourtifyRights {
 		uint256 _petitonerID,
 		string memory _name,
 		uint256 _caseType
-	) public onlyCourt {
-		_createNewCase(_timestamp, _state, _district, _court, _petitonerID, _name, _caseType);
+	) public onlyCourt returns(uint256 _newCaseID){
+        _newCaseID = _createNewCase(_timestamp, _state, _district, _court, _petitonerID, _name, _caseType);
 	}
 
 	/*
-	 *
+	 * @notice Function to upload the evidence.
+     * @param _caseID The case ID for which the evidence will be added.
+     * @param _IPFS The IPFS hash.
 	 */
 	function uploadEvidence(uint256 _caseID, string memory _IPFS) public onlyAdvocate {
 		_uploadEvidence(_caseID, _IPFS);
@@ -85,7 +108,15 @@ contract Courtify is CourtifyRights {
 	/* INTERNAL */
 
 	/*
-	 *
+     * @param _timestamp The time when the case was created.
+     * @param _state The state where the case was created.
+     * @param _district The district where the case was created.
+     * @param _court The court where the case was created.
+     * @param _petitonerID The Petitioner ID.
+     * @param _name The Petitioner Name.
+     * @param _caseType The case type.
+     * @return _newCaseID The new case id which was created.
+     * @dev The internal function which creates a new case.
 	 */
 	function _createNewCase(
 		uint256 _timestamp,
@@ -95,8 +126,9 @@ contract Courtify is CourtifyRights {
 		uint256 _petitonerID,
 		string memory _name,
 		uint256 _caseType
-	) internal {
-		Case memory _case = cases[lastCaseID];
+	) internal returns(uint256 _newCaseID){
+        _newCaseID = lastCaseID;
+		Case storage _case = cases[_newCaseID];
 		_case.timestamp = _timestamp;
 		if (_timestamp == 0) {
 			_case.timestamp = block.timestamp;
@@ -113,12 +145,14 @@ contract Courtify is CourtifyRights {
 			_case.petitonerID = _petitonerID;
 		}
 		_case.caseType = _caseType;
-		emit CaseCreated(msg.sender, lastCaseID, _timestamp, _state, _district, _court, _petitonerID, _caseType);
+		emit CaseCreated(msg.sender, _newCaseID, _timestamp, _state, _district, _court, _petitonerID, _caseType);
 		lastCaseID++;
 	}
 
 	/*
-	 *
+     * @param _caseID The case ID for which the evidence will be added.
+     * @param _IPFS The IPFS hash.
+     * @dev Internal function to upload the evidence.
 	 */
 	function _uploadEvidence(uint256 _caseID, string memory _IPFS) internal {
 		cases[_caseID].evidence.push(_IPFS);
@@ -128,7 +162,14 @@ contract Courtify is CourtifyRights {
 	/* GETTER */
 
 	/*
-	 *
+	 * @notice Function to get the case details.
+     * @param _caseID The case ID which has to be queried.
+     * @return _timestamp The time when the case was created.
+     * @return _state The state where the case was created.
+     * @return _district The district where the case was created.
+     * @return _court The court where the case was created.
+     * @return _name The Petitioner Name.
+     * @return _caseType The case type.
 	 */
 	function getCase(uint256 _caseID)
 		public
@@ -148,7 +189,9 @@ contract Courtify is CourtifyRights {
 	}
 
 	/*
-	 *
+	 * @notice Function to get the evidence of a case.
+     * @param _caseID The case ID which has to be queried.
+     * @return The IPFS array of evidences for a case.
 	 */
 	function getEvidence(uint256 _caseID) public view returns (string[] memory) {
 		return cases[_caseID].evidence;
